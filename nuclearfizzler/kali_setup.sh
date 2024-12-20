1e322698a17b4f308a9c54152620cae057bf9514
NuclearFizzler <nuclearfizzler@gmail.com>
2024-12-20 14:19:40 -0800

    Add Kali setup

---
+++ nuclearfizzler/kali_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt install enum4linux-ng netexec impacket impacket-scripts tmux flameshot xclip
+
+
+
+# start bloodhound
+# sudo neo4j console
+# sudo neo4j-admin dbms set-initial-password kali # what happened to neo4j-admin??
+
