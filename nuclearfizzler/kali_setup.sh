ad581654f962ea2e0ef19664a7d9d5fd98b44b1e
NuclearFizzler <nuclearfizzler@gmail.com>
2024-12-30 22:11:42 -0800

    More resources for me

--- nuclearfizzler/kali_setup.sh
+++ nuclearfizzler/kali_setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 sudo apt update
-sudo apt install enum4linux-ng netexec impacket impacket-scripts tmux flameshot xclip
+sudo apt -y install enum4linux-ng netexec impacket impacket-scripts tmux flameshot xclip peass
 
+# Download github resources
+wget -P ~/ https://raw.githubusercontent.com/nationalcptc-teamtools/University-of-California-Davis/refs/heads/main/nuclearfizzler/.tmux.conf
 
+wget -P ~/ https://raw.githubusercontent.com/61106960/adPEAS/refs/heads/main/adPEAS-Light.ps1
 
-# start bloodhound
-# sudo neo4j console
-# sudo neo4j-admin dbms set-initial-password kali # what happened to neo4j-admin??
+# Setup neo4j for bloodhound
+sudo neo4j console 1>/dev/null &
+sleep 5
+firefox http://localhost:7474/browser/
 
